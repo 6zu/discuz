8a2583f6d47a002f9d44a195f70e5f2cbbd88e0d
hertar <809298222@qq.com>
2014-12-10 20:33:38 +0800

    shs Signed-off-by: hertar <809298222@qq.com>

--- data/sysdata/cache_adminextend.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Discuz! cache file, DO NOT modify me!
-//Identify: 9eeed6641a649de17bf67599759dca78
-
-$adminextend = array (
-  0 => 'cloud.php',
-);
-?>
--- data/sysdata/cache_pluginsetting.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Discuz! cache file, DO NOT modify me!
-//Identify: 3ea9e960d3c0828d417ec00a6788ffde
-
-$pluginsetting = array (
-);
-?>
